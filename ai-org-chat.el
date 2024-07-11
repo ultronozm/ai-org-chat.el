59e2f135588411bfb78474aff958d8aaa74bd70e
Paul Nelson <ultrono@gmail.com>
2024-07-11 16:55:08 +0200

    bump version to 0.1

--- ai-org-chat.el
+++ ai-org-chat.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2023  Paul D. Nelson
 
 ;; Author: Paul D. Nelson <nelson.paul.david@gmail.com>
-;; Version: 0.0
+;; Version: 0.1
 ;; URL: https://github.com/ultronozm/ai-org-chat.el
 ;; Package-Requires: ((emacs "29.1") (gptel "0.9.0"))
 ;; Keywords: convenience, ai, chat
